04ccf5114b9718953abd7e4a63a4f8dd69583104
Valercr <rojasvalen94@gmail.com>
2026-07-05 23:48:54 -0600

    cambio en  main

--- app/main.py
+++ app/main.py
@@ -1,18 +1,22 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from app.core.database import Base, engine
 from app.api.routes import auth, items
 
-# Crear app
-app = FastAPI(title="FastAPI CRUD Auth")
 
-# Crear tablas en DB (solo para desarrollo)
-Base.metadata.create_all(bind=engine)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
 
-# Registrar rutas
+app = FastAPI(
+    title="FastAPI CRUD Auth",
+    lifespan=lifespan
+)
+
 app.include_router(auth.router)
 app.include_router(items.router)
-
 
 @app.get("/")
 def root():
