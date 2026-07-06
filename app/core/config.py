8610d12faa340cd16315f2c55d25071224261e9a
Valercr <rojasvalen94@gmail.com>
2026-07-05 23:42:17 -0600

    cambio en  database url

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv() 
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/fastapi_db")
+#DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/fastapi_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL no está definida")
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
